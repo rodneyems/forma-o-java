016333abbcecc42817e6e03e18ff974df8b39e4f
Rodney Martins <rodney.martins@live.com>
2021-06-07 22:32:20 -0300

    Operadores lógicos

---
+++ Compras.java
@@ -0,0 +1,23 @@
+class Compras{
+
+    public static void main(String args[]){
+        double valorCompra = 2000;
+        double valorAPagar = valorCompra;
+        int quantidade = 4;
+
+        if (valorCompra > 1000 && quantidade > 3){
+            valorAPagar = valorCompra * 0.9;
+        }else if (valorCompra > 500){
+            valorAPagar = valorCompra * 0.95
+        }
+    }
+
+}
+
+// Operadores
+// 1. Aritméticos: + - * / %
+// 2. Concatenação: +
+// 3. Comparação: > >= < <= == !=
+// && = AND
+// || = OR
+// Teste de mesa = ao teste de lógica da sua regra de negocio
