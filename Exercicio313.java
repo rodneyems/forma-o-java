c011afb37dba10aa806f37cb0f9e32d9e43fb57c
Rodney Martins <rodney.martins@live.com>
2021-06-07 23:03:23 -0300

    Exercicios 3.13

---
+++ Exercicio313.java
@@ -0,0 +1,20 @@
+class Exercicio313{
+
+    public static void main(String args[]){
+        // 1
+        // for(int i = 150; i <= 300; i++){
+        //     System.out.println(i);
+        // }
+
+        // 2
+        // int soma = 0;
+        // for(int i = 1; i <= 1000; i++){
+        //     soma += i;
+        //     System.out.println(soma);
+        // }
+
+        // 3
+        
+    }
+
+}
