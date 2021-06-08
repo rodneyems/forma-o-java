7372b2f6a89376db1677519a9cbde845f69debac
RENEAS <rodney.martins@live.com>
2021-06-08 20:08:50 -0300

    Fim dos exercicios 3.13

--- Exercicio313.java
+++ Exercicio313.java
@@ -14,6 +14,36 @@ class Exercicio313{
         // }
 
         // 3
+        // for(int i = 3; i <= 100; i += 3){
+        //     System.out.println(i);
+        // }
+        
+        // 4
+        // int fatorial = 1;
+        // for (int n = 1; n <= 10; n++){
+        //     fatorial *= n;
+        //     System.out.println(fatorial);
+        // }
+
+        // 5
+        // long fatorial = 1;
+        // for (long n = 1; n <= 40; n++){
+        //     fatorial *= n;
+        //     System.out.println(fatorial);
+
+        //6
+        // int anterior = 0;
+        // int atual = 1;
+        // String fibo = "A SEQUÊNCIA É: 0, 1";
+        // while (atual < 100){
+        //     int proximo = anterior + atual;
+        //     fibo += ", " + proximo;
+        //     anterior = atual;
+        //     atual = proximo;
+        // }
+        // System.out.println(fibo);
+
+        //7
         
     }
 
