ea89dd775a0a2ab8ca732b59e86cbe84c38df3f5
Rodney Martins <rodney.martins@live.com>
2021-06-07 21:56:59 -0300

    Exercicio 3.3 pag 18

---
+++ Exercicio33.java
@@ -0,0 +1,13 @@
+class Exercicio33{
+
+    public static void main(String args[]){
+        int jan = 15000;
+        int fev = 23000;
+        int mar = 17000;
+        int total = jan + fev + mar;
+        int media = total / 3;
+        System.out.println("Total de gastos: " + total);
+        System.out.println("A media de gastos foi de: " + media);
+    }
+
+}
