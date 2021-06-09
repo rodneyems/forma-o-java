9a3a15386c421c614d727e0d9bf50668b25fc302
RENEAS <rodney.martins@live.com>
2021-06-08 23:37:59 -0300

    DIA 2 - Exercicio OO 4.12

--- Dia 2/Conta.java
+++ Dia 2/Conta.java
@@ -5,15 +5,30 @@ class Conta{
   int numero;
   String agencia;
   double saldo;
+  String dataAbertura;
 
   void deposita(double valor){
     this.saldo += valor;
   }
+
+  void saca(double valor){
+    this.saldo -= valor;
+  }
+
   double extrato(){
     return this.saldo;
   }
+
+  double calculaRendimento(){
+    return saldo * 0.1;
+  }
+
   void transferecia(double valor, Conta contaDestino){
     this.saldo -= valor;
     contaDestino.saldo += valor;
+  }
+
+  String recuperaDadosParaImpressao(){
+    return "titular: " + this.titular + "AG e Conta: " + this.agencia + "/" + this.numero + "saldo: " + this.saldo;
   }
 }
---
+++ Dia 2/Exercicio412.java
@@ -0,0 +1,34 @@
+class Exercicio412{
+  public static void main (String args[]){
+    Conta c = new Conta ();
+    c.titular = "Rodney";
+    c.numero = 1234;
+    c.agencia = "ag12";
+    c.saldo = 2000;
+
+
+    Conta c1 = new Conta ();
+    c1.titular = "Jose";
+    c1.numero = 5678;
+    c1.agencia = "ag50";
+    c1.saldo = 5000;
+
+    System.out.println("ANTES Rodney: " + c.saldo);
+    System.out.println("ANTES Jose: " + c1.saldo);
+
+    c1.transferecia(2000, c);
+
+    System.out.println("DEPOIS Rodney: " + c.saldo);
+    System.out.println("DEPOIS Jose: " + c1.saldo);
+
+    System.out.println("RENDIMENTO Rodney: " + c1.calculaRendimento());
+    System.out.println("RENDIMENTO Rodney: " + c.calculaRendimento());
+
+    c1.saca(500);
+    System.out.println("DEPOIS Jose: " + c1.saldo);
+
+    System.out.println("IMPRIMINDO: " + c1.recuperaDadosParaImpressao());
+
+  }
+
+}
