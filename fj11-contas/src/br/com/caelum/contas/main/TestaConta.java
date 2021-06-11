3dd061b387ffe282775b2f76a0662f84ed1bc1ac
RENEAS <rodney.martins@live.com>
2021-06-11 20:55:42 -0300

    Dia 4 e 5 - Eclipse

---
+++ fj11-contas/src/br/com/caelum/contas/main/TestaConta.java
@@ -0,0 +1,12 @@
+package br.com.caelum.contas.main;
+import br.com.caelum.contas.modelo.Conta;
+
+public class TestaConta {
+
+	public static void main(String[] args) {
+		Conta c = new Conta();
+		c.setTitular("Rodney");
+		System.out.println(c.getTitular());
+	}
+
+}
